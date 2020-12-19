a68bad168780c22174ecd612e4d1d751e3dbd881
ibrahimbayyinah <75299537+ibrahimbayyinah@users.noreply.github.com>
2020-12-19 19:36:49 +0100

    Create testing_basics.java

---
+++ java/testing_basics.java
@@ -0,0 +1,13 @@
+class MyClass {
+    public static void main(String[ ] args) {
+        String name ="David";
+        int age = 42, children = 3;
+        double score =15.9;
+        char group = 'Z';
+        boolean diabetes = true, married = false;
+        System.out.print(name);
+        System.out.println(age);
+        System.out.println("Is " + name + " married?");
+        System.out.println(married);
+    }
+}
