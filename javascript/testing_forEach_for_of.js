5b39b4c4a24978154422a71fdcbccaf6c5d66f05
ibrahimbayyinah <75299537+ibrahimbayyinah@users.noreply.github.com>
2020-12-19 14:15:11 +0100

    Create testing_forEach_for_of.js

---
+++ javascript/testing_forEach_for_of.js
@@ -0,0 +1,19 @@
+/* var arr = [2, 3, 7, 8];
+
+arr.forEach(function(el) {
+    console.log(el * 2);
+}); */
+
+const arr = [2, 3, 7, 8];
+
+arr.forEach(el => {
+    console.log(el * 2);
+})
+
+console.log("\n");
+
+let arr2 = [3, 4, 8, 9];
+
+for (let num of arr2){
+    console.log(num * 2);
+}
