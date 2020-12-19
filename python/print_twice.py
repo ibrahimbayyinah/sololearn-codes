d27c1b6892cdd018f583341c6ccb137df1111cf1
ibrahimbayyinah <75299537+ibrahimbayyinah@users.noreply.github.com>
2020-12-19 19:31:54 +0100

    Create print_twice.py

---
+++ python/print_twice.py
@@ -0,0 +1,10 @@
+def print_sum_twice(x,y):
+    print((str(x+y)+'\n')*2)
+
+print_sum_twice(3,5)
+
+def print_sum_twice2(x, y):
+    sum = x+y
+    print(sum, sum, sep='\n')
+
+print_sum_twice2(3, 5)
