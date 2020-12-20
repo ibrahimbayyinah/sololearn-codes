8f75f0409d15fe6f20eab02ea3667b5e53856040
ibrahimbayyinah <75299537+ibrahimbayyinah@users.noreply.github.com>
2020-12-21 00:35:13 +0100

    Update ael.js

--- javascript/testing_addEventListener2/ael.js
+++ javascript/testing_addEventListener2/ael.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     var slider = document.getElementById("img");
-    pictures = ["http://www.sololearn.com/uploads/slider/1.jpg", "http://www.sololearn.com/uploads/slider/2.jpg", "http://www.sololearn.com/uploads/slider/3.jpg"];
+    pictures = ["https://cdn.pixabay.com/photo/2020/02/13/19/19/winter-4846638_960_720.jpg", "https://cdn.pixabay.com/photo/2018/03/28/00/31/winter-3267925_960_720.jpg", "https://cdn.pixabay.com/photo/2020/12/03/05/23/lion-5799523_960_720.jpg"];
     var prev = document.getElementById("prev");
     var next = document.getElementById("next");
     var num = 0;
