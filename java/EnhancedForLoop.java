0932d9061649260f83a24ddeb00add255b5a9613
ibrahimbayyinah <75299537+ibrahimbayyinah@users.noreply.github.com>
2020-12-21 00:01:09 +0100

    Create EnhancedForLoop.java

---
+++ java/EnhancedForLoop.java
@@ -0,0 +1,9 @@
+class MyProgram {
+    public static void main(String[] args) {
+        int[] myArr = {2, 3, 8, 5, 7};
+        
+        for (int t: myArr) {
+            System.out.println(t);
+        }
+    }
+}
